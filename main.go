fb98e3f64191949cb89c667d42a5274bbda24ac3
Systemnick <systemnick@volginsky.net>
2019-09-23 09:54:35 +0000

    Add application stopping by os signal logic

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -13,5 +18,23 @@ func main() {
 
 	fmt.Printf("Application: %+v\n", a)
 
-	a.Run()
+	go func() {
+		if err := a.Run(); err != http.ErrServerClosed {
+			a.Logger().Fatal().Msgf("http server run error: %s", err)
+			return
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	a.Logger().Info().Msg("get signal start to shutdown")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := a.Stop(ctx); err != nil {
+		a.Logger().Error().Msgf("application stop error %s", err)
+		return
+	}
 }
